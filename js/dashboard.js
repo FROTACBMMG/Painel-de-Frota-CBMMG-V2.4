43adf2cdd1bb0ecf1143e4d05c13bb29297f5bfc
FROTACBMMG <155545159+FROTACBMMG@users.noreply.github.com>
2026-08-05 09:25:14 -0300

    Remove old comments and clean up code formatting

--- js/dashboard.js
+++ js/dashboard.js
@@ -173,7 +173,7 @@ function atualizarCombustivel(dados) {
 }
 
 //==================================================
-// Indicadores coloridos
+// Barras Indicadores
 //==================================================
 
 function atualizarBarrasIndicadores(dados){
@@ -198,20 +198,17 @@ function atualizarBarrasIndicadores(dados){
     document.getElementById("barraDisponiveis").style.width =
         percDisponiveis + "%";
 
-    document.getElementById("txtBarraDisponiveis").textContent =
-    (disponiveis/total*100).toFixed(1) + "%";
+   
 
     document.getElementById("barraManutencao").style.width =
         percManutencao + "%";
 
-    document.getElementById("txtBarraManutencao").textContent =
-    (manutencao/total*100).toFixed(1) + "%";
+   
 
     document.getElementById("barraDescarga").style.width =
         percDescarga + "%";
 
-    document.getElementById("txtBarraDescarga").textContent =
-    (descarga/total*100).toFixed(1) + "%";
+   
 
     atualizarTexto(
         "percentualDisponiveis",
